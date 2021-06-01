c9383c882f06019db699564dae6fd95a1cafda9b
alaabaroud <alaabaroud783@gmail.com>
2021-06-01 08:39:17 +0300

    day2

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 let hours= ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
 
 
-let Seattle={
+/*let Seattle={
   branchName: 'Seattle',
   minCust:23,
   maxCust:65,
@@ -12,9 +12,8 @@ let Seattle={
   cust1: [],
   custArr:function () {
     for (let i= 0; i < hours.length; i++){
-      this.cust1.push(randomNumber(this.minCust,this.maxCust));
+      this.cust1.push(randomNumber(this.minCust,this.maxCust)*this.avgCookies);
     }
-    
   }
 };
 Seattle.custArr();
@@ -24,7 +23,37 @@ console.log( Seattle.cust1);
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-
+*/
 //console.log(seattle);
 //console.log(Seattle.cust);
+// this is from w3schooles
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function LocationName(branchName, minCust, maxCust, avgCookie) {
+  this.branchName= branchName;
+  this.minCust=minCust;
+  this.maxCust=maxCust;
+  this.avgCookie=avgCookie;
+  this.customers=[];
+  this.cookies=[];
+}
+LocationName.prototype.cust1=function () {
+  for (let i= 0; i < hours.length; i++){
+    this.customers.push(random(this.minCust,this.maxCust));
+  }
+};
+LocationName.prototype.cookies1=function () {
+  for (let index = 0; index <14; index++) {
+    this.cookies.push(random(this.customers,this.avgCookie));
+  }
+};
+
+let seattle= new LocationName ('seattle', 23, 65, 6.3);
+seattle.cookies1();
+//seattle.cust1();
+console.log(seattle);
+
+//let parent=document.getElementById('parent');
 
